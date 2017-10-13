8c4c267d5f3c9f5810c54efef90c5f241dc3b597
ApTypp <32418088+ApTypp@users.noreply.github.com>
2017-10-13 19:08:34 +0300

    Read button fixed

--- index.php
+++ index.php
@@ -18,15 +18,15 @@ include_once (getRoot('/template/header.php')); // Header ?>
                     </button>
                     <div class="dropdown-menu ">
                         <a class="dropdown-item" href="read_post.php?id=<?php echo $post['id'] ?>">Open</a>
-                        <a class="dropdown-item" data-toggle="modal" data-target="#exampleModal">Read</a>
+                        <a class="dropdown-item" data-toggle="modal" data-target="#Modal<?php echo $post['id'];?>">Read</a>
                         <a class="dropdown-item" href="edit_post.php?id=<?php echo $post['id'] ?>">Edit</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="delete_post.php?id=<?php echo $post['id'] ?>"> <!-- onclick="return confirm('Are you sure?')" -->Delete</a>
                     </div>
                 </div>
                 <div class="container">
-                    <h1 class="display-6"><?php echo htmlspecialchars( $post['title']);?> </h1>
-                    <p class="lead"><?php echo htmlspecialchars( $post['body']);?> </p>
+                    <h1 class="display-6" style="text-align: center"><?php echo htmlspecialchars( $post['title']);?> </h1>
+                    <p class="lead" style="text-align: center"><?php echo htmlspecialchars( $post['body']);?> </p>
                 </div>
                 <div class="modal-footer"><?php
                     if ($post['date_modified'] == NULL){
@@ -36,18 +36,28 @@ include_once (getRoot('/template/header.php')); // Header ?>
                         echo 'Modified: '.$post['date_modified'];
                     } ?>
                 </div>
+                <div class="form-group" style="margin-left: 5%; margin-right: 5%">
+                    <textarea class="form-control" id="comment" rows="1" name="title" placeholder="Comment"></textarea>
+                </div>
+                <div class="modal-footer">
+                    <form action="insert_comment.php" method="post">
+                        <button type="submit" class="btn btn-primary">Comment</button>
+                    </form>
+
+                </div>
             </div>
-            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <!-- Modal (read post) -->
+            <div class="modal fade" id="Modal<?php echo $post['id']; ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel"><?php echo htmlspecialchars( $post['title']);?></h5>
+                            <h5 class="modal-title" id="exampleModalLabel"><?php echo htmlspecialchars($post['title']);?></h5>
                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
                         <div class="modal-body">
-                            <?php echo htmlspecialchars( $post['body']);?>
+                            <?php echo htmlspecialchars($post['body']);?>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
