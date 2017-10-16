e3ae365a8feab62bd6d73ef20a1b0a0cfa72e9b3
ApTypp <32418088+ApTypp@users.noreply.github.com>
2017-10-16 21:23:01 +0300

    some corrections

--- search.php
+++ search.php
@@ -4,19 +4,19 @@ include_once (getRoot('/template/header.php')); // Header ?>
 
 <div class="container">
 <?php
-$queryy = 'SELECT p.title, p.body, c.author, c.comments_body
+$query = 'SELECT p.title, p.body, c.author, c.comments_body
 FROM posts AS p
 JOIN comments AS c ON p.id = c.post_id
 WHERE p.body LIKE ? OR p.title LIKE ? OR c.author LIKE ? OR c.comments_body LIKE ?';
-$stmt = $dbal->prepare($queryy);
+$stmt = $dbal->prepare($query);
 $stmt->execute(['%'.$_POST['search'].'%','%'.$_POST['search'].'%','%'.$_POST['search'].'%','%'.$_POST['search'].'%']);
 $i = 1;
-while ($resultt = $stmt->fetch()){
+while ($result = $stmt->fetch()){
     echo 'Result #'.$i.': <br />';
-    echo 'Post title : '.htmlspecialchars($resultt['title']).'<br />';
-    echo 'Post body: '.htmlspecialchars( $resultt['body']).'<br />';
-    echo 'Comment author: '.htmlspecialchars( $resultt['author']).'<br />';
-    echo 'Comment: '.htmlspecialchars( $resultt['comments_body']).'<br />';
+    echo 'Post title : '.htmlspecialchars($result['title']).'<br />';
+    echo 'Post body: '.htmlspecialchars( $result['body']).'<br />';
+    echo 'Comment author: '.htmlspecialchars( $result['author']).'<br />';
+    echo 'Comment: '.htmlspecialchars( $result['comments_body']).'<br />';
     echo '<br />';
     $i++;
 }
